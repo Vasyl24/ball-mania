27bfb234341a183f351ef1baa3c37defe07522b5
Vasyl Shaposhnyk <aries240302@gmail.com>
2024-03-08 22:35:09 +0200

    Add two images and style

--- src/components/Overstock/Overstock.styled.jsx
+++ src/components/Overstock/Overstock.styled.jsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
-import img from '../../assets/overstock.jpg';
+import img from '../../assets/overstock-mob.png';
 
 export const Image = styled.section`
   width: 100%;
-  background-size: cover;
-  background-image: linear-gradient(#2e2f42b3, #2e2f42b3), url(${img});
-
+  height: 194px;
+  background-image: url(${img});
+  //   background-image: linear-gradient(#2e2f42b3, #2e2f42b3), url(${img?.src});
+  background-color: #2e2f42;
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
